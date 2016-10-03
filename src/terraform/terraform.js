2b2829b47c5c54f3bcf5d739150589041be83058
Vaseem Abbas Mohammed <vaseemabbas.mohammed@gmail.com>
2016-10-03 15:59:41 +0530

    Tech:Vaseem/Selva:Terraform provisioning commands execution

--- src/terraform/terraform.js
+++ src/terraform/terraform.js
@@ -1,23 +1,24 @@
 var fs = require("fs");
+var sh = require("shelljs");
 
-var terraform = 'variable "azure_settings_file" {'+
-'default = "./common/settings/dev"'+
-'}'+
-''+
-'provider "azure" {'+
-'    publish_settings = "${file(var.azure_settings_file)}"'+
-'}'+
+var terraform = 'variable "azure_settings_file" {\n'+
+    '\t default = "./common/settings/dev"\n'+
+    '}\n'+
+    '\n'+
+    'provider "azure" {\n'+
+    ' \t   publish_settings = "${file(var.azure_settings_file)}"\n'+
+    '}\n'+
 
-'module ${appname} {'+
-'    source = "https://vaseems@bitbucket.org/prosto-vsapkrmkad/terraform-modules.git"'+
-'    name = "${appname}"'+
-'    size = "Standard_DS3_v2"'+
-'}';
+    'module ${appname} {\n'+
+    ' \t   source = "https://vaseems@bitbucket.org/prosto-vsapkrmkad/terraform-modules.git"\n'+
+    ' \t   name = "${appname}"\n'+
+    '  \t  size = "Standard_DS3_v2"\n'+
+    '}';
 
 
 function generateTerraformFile(appName){
     var path = "main.tf";
-    var data = terraform.replace('${appname}', appName);
+    var data = terraform.replace(/\$\{appname\}/g, appName);
 
     fs.writeFile(path, data, function(error) {
         if (error) {
@@ -26,4 +27,11 @@ function generateTerraformFile(appName){
             console.log("Successful Write to " + path);
         }
     });
+}
+
+function provisionVM(){
+    console.log("Initiating the VM provisions");
+    sh.exec("terraform plan");
+    console.log("Provisioning the VMs");
+    sh.exec("terraform apply");
 }
